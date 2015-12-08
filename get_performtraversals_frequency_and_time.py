f411494998af99a8d3b8c9ea3c506c77a4174db8
wangsiyuan03 <wangsiyuan03@baidu.com>
2015-12-08 13:16:52 +0800

    add get_longest_frame_length to performTraversals for fps calculate

--- get_performtraversals_frequency_and_time.py
+++ get_performtraversals_frequency_and_time.py
@@ -7,9 +7,13 @@ def get_performtraversals_time_list(app_name,minimum_time,start_time,end_time,ti
     time_list = []
     child_func_number = 0
     current_time = 0
+    longest_frame_length = 0
+    longest_frame_start_time = 0
+    start_time_offset = 0
     for line in open(app_name + '_process_trace.txt'):
         items = line.split()
         if len(items) == 1:
+            start_time_offset = float(items[0])	
             start_time += float(items[0])
             end_time += float(items[0])
             continue
@@ -24,16 +28,20 @@ def get_performtraversals_time_list(app_name,minimum_time,start_time,end_time,ti
             if child_func_number == 0:
                 delta_time = time - current_time
                 time_tuple = (current_time,delta_time)
+                if longest_frame_length < delta_time:
+                    longest_frame_length = delta_time
+                    longest_frame_start_time = current_time					
                 if delta_time >= minimum_time and current_time >= start_time and current_time <= end_time:
                     time_list.append(time_tuple)
             else:
                 child_func_number -= 1
         #else:
         #    print last_item
-    return time_list
+    longest_frame_start_time -= start_time_offset	
+    return time_list,longest_frame_start_time,longest_frame_length
 
 
-def write_performtraversals_time_file(time_list,app_name,minimum_time,delta_time):
+def write_performtraversals_time_file(time_list,app_name,minimum_time,delta_time,longest_frame_start_time,longest_frame_length):
     time_file = file(app_name + '_performtraversals_' + str(int(minimum_time*1000)) +'.txt','w')
     for items in time_list:
         time_file.write(str(items[0]) + '\t' + str(items[1]) + '\n')
@@ -41,6 +49,8 @@ def write_performtraversals_time_file(time_list,app_name,minimum_time,delta_time
     frequency = total_number * 1.0 / delta_time
     time_file.write('Total:\t' + str(len(time_list)) + '\n')
     time_file.write('Frequency:\t' + str(frequency) + '\n')
+    time_file.write('Longest frame start time:\t' + str(longest_frame_start_time) + '\n')
+    time_file.write('Longest frame length:\t' + str(longest_frame_length) + '\n')	
 
 def read_configs_and_run():
     config = ConfigParser.ConfigParser()
@@ -54,8 +64,8 @@ def read_configs_and_run():
     #travert unit to 's'
     minimum_time /= 1000
     #TODO get start_time and end_time
-    time_list = get_performtraversals_time_list(app_name,minimum_time,start_time,end_time,time_index)
-    write_performtraversals_time_file(time_list,app_name,minimum_time,end_time - start_time)
+    time_list,longest_frame_start_time,longest_frame_length = get_performtraversals_time_list(app_name,minimum_time,start_time,end_time,time_index)
+    write_performtraversals_time_file(time_list,app_name,minimum_time,end_time - start_time,longest_frame_start_time,longest_frame_length)
 
 if __name__ == '__main__':
     read_configs_and_run()
